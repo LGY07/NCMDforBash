8af9ff33db344abcdc9039c45bacc1aba173920b
UrsusFeline <84217988+LGY07@users.noreply.github.com>
2022-04-30 22:34:55 +0800

    Update ncmd.sh

--- ncmd.sh
+++ ncmd.sh
@@ -7,6 +7,7 @@ if [ "$?" != "0" ]; then
     echo -e "\033[33mDo you want to try installing wget?\033[0m"
     read -p "[Y/n](default=n)" install
     if [[ $install == "Y" || $install == "y" ]];then
+        if [[ $(uname -o) == "Android" ]];then apt-get update -y;apt-get install wget -y && bash $0 $1 $2
         hash dnf > /dev/null 2>&1
         if [ "$?" == "0" ]; then dnf check-update -y;sudo dnf install wget -y && bash $0 $1 $2
         fi
@@ -14,7 +15,7 @@ if [ "$?" != "0" ]; then
         if [ "$?" == "0" ]; then sudo pacman -Sy wget --noconfirm && bash $0 $1 $2
         fi
         hash apt-get > /dev/null 2>&1
-        if [ "$?" == "0" ]; then apt-get update wget -y;sudo apt-get install wget -y && bash $0 $1 $2
+        if [ "$?" == "0" ]; then apt-get update -y;sudo apt-get install wget -y && bash $0 $1 $2
         fi
     fi
 exit
@@ -27,7 +28,7 @@ echo "usage: $0 [options] COMMAND"
 echo ""
 echo "List of Main Commands:"
 echo ""
-echo "-h    --help		display a helpful usage message"
+echo "-h    --help      display a helpful usage message"
 echo "-d    --download  download a song by Song ID"
 echo ""
 echo "If no parameters are added, the default interaction mode."
@@ -50,6 +51,6 @@ read -p "Song ID:" songid
 echo "Downloading..."
 wget 'https://music.163.com/song/media/outer/url?id='$songid'.mp3' 2>> NCMD.log
 if [ $? == 0 ];then
-echo -e "\033[33mDone!\033[0m" && mv 'url?id='$2'.mp3' $2'.mp3'
+echo -e "\033[33mDone!\033[0m" && mv 'url?id='$songid'.mp3' $songid'.mp3'
 else echo -e "\033[31mERROR:\033[0mPlease check the log file NCMD.log"
 fi
